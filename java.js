5e0f9f94e5805094ff2c8b35645ce1203d80b848
amani A <bella0060@yahoo.com>
2020-08-13 08:49:38 +0200

    first commit

---
+++ java.js
@@ -0,0 +1,20 @@
+var grade=77
+if(grade>90 && grade<=100)
+{
+    console.log('Excellent')
+}
+else if(grade>80 && grade<=90)
+{
+    console.log('very good')
+}
+else if(grade<=80 && grade>70)
+{
+    console.log('good')
+}
+else if(grade<=70 && grade>=60)
+{
+    console.log('pass')
+}
+else{
+console.log('failed')
+}
